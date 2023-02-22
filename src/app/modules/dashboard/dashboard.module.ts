a33e8a44ef8be5af3e75f0362fefcd62f555744a
Fernanda Montt Muñoz <mfmontt@sacyr.com>
2023-02-22 19:30:49 -0300

    sidenav

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -9,7 +10,8 @@ import { HomeComponent } from './components/home/home.component';
     DashboardComponent,
     HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class DashboardModule { }

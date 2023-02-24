df7b79d84d2cadab108cdd14073223388e2d7849
Fernanda Montt Muñoz <mfmontt@sacyr.com>
2023-02-24 18:23:19 -0300

    buscar categoria

--- src/app/modules/shared/services/category.service.ts
+++ src/app/modules/shared/services/category.service.ts
@@ -40,6 +40,13 @@ export class CategoryService {
     const endpoint = `${base_url}/categories/ ${id}`;
     return this.http.delete(endpoint);
   }
+  /**
+   * search categorias
+   */
+  getCategorieById(id:any){
+    const endpoint = `${base_url}/categories/ ${id}`;
+    return this.http.get(endpoint);
+  }
 
 
 }
